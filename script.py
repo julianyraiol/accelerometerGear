43ff5f7ce4d008f4fdea8e233a7d3dce9fb442c7
Juliany Raiol <jrr.snf@uea.edu.br>
2018-01-14 00:14:24 -0400

    chore: remove function create_file

--- script.py
+++ script.py
@@ -1,19 +1,4 @@
 import paho.mqtt.client as mqtt
-import csv 
-import json as js	
-
-def create_file(j):
-    
-    csv = open('data.csv', 'w')
-    column = 'x, y, z'
-    csv.write(column)
-    
-    #json = js.loads(json)
-    ju = js.loads(j.encode('utf-8'))
-    teste = ju['x']
-    print teste
-#    x = j['x'], y = j['y'], z = j['z']
-#    print x + " " + y + " " + z    
 	    
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
